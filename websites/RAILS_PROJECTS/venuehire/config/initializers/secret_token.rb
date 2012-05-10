e9e73fc6e4437b42979513ee086286ff3e8bed50
nick <nickholland@wtmidlands.com>
2012-05-10 16:09:24 +0100

    Initial commit

---
+++ websites/RAILS_PROJECTS/venuehire/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venuehire::Application.config.secret_token = '494dbacd7eb70a2bf9818d20f4a016fe0489bf704950587e0c425d57e31a06f805d2ee6601505bb4fe9caa777a51bc0fed6ad32cd65f68002c5b823fa8b2739c'
